78c4b8cb7eb021549c1b135ae2a4e2ed3b1dfd36
Nikhil Dilip <nikhil@stytch.com>
2021-09-29 18:46:34 -0700

    lint

--- stytch/attributes.go
+++ stytch/attributes.go
@@ -23,25 +23,25 @@ type Options struct {
 }
 
 type EmailFactor struct {
-	EmailID string `json:"email_id,omitempty"`
+	EmailID      string `json:"email_id,omitempty"`
 	EmailAddress string `json:"email_address,omitempty"`
 }
 
 type PhoneNumberFactor struct {
-	PhoneID string `json:"phone_id,omitempty"`
+	PhoneID     string `json:"phone_id,omitempty"`
 	PhoneNumber string `json:"phone_number,omitempty"`
 }
 
 type GoogleOAuthFactor struct {
-	ID string `json:"id,omitempty"`
-	EmailID string `json:"email_id,omitempty"`
+	ID              string `json:"id,omitempty"`
+	EmailID         string `json:"email_id,omitempty"`
 	ProviderSubject string `json:"provider_subject,omitempty"`
 }
 
 type AuthenticationFactor struct {
-	Type string `json:"type,omitempty"`
-	DeliveryMethod string `json:"delivery_method,omitempty"`
-	EmailFactor EmailFactor `json:"email_factor,omitempty"`
+	Type              string            `json:"type,omitempty"`
+	DeliveryMethod    string            `json:"delivery_method,omitempty"`
+	EmailFactor       EmailFactor       `json:"email_factor,omitempty"`
 	PhoneNumberFactor PhoneNumberFactor `json:"phone_number_factor,omitempty"`
 	GoogleOAuthFactor GoogleOAuthFactor `json:"google_oauth_factor,omitempty"`
 }
@@ -50,11 +50,11 @@ type AuthenticationFactor struct {
  * Structure for the custom type Session
  */
 type Session struct {
-	SessionID      string     `json:"session_id,omitempty"`
-	UserID         string     `json:"user_id,omitempty"`
-	StartedAt      string     `json:"started_at,omitempty"`
-	LastAccessedAt string     `json:"last_accessed_at,omitempty"`
-	ExpiresAt      string     `json:"expires_at,omitempty"`
-	Attributes     Attributes `json:"attributes,omitempty"`
+	SessionID             string                  `json:"session_id,omitempty"`
+	UserID                string                  `json:"user_id,omitempty"`
+	StartedAt             string                  `json:"started_at,omitempty"`
+	LastAccessedAt        string                  `json:"last_accessed_at,omitempty"`
+	ExpiresAt             string                  `json:"expires_at,omitempty"`
+	Attributes            Attributes              `json:"attributes,omitempty"`
 	AuthenticationFactors []*AuthenticationFactor `json:"authentication_factors,omitempty"`
 }
