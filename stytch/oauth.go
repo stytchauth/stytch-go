5529323515a670b5504b5d5638ced6f7a55cb379
Danny Thomson <danny@stytch.com>
2022-04-21 09:48:34 -0700

    Make expires_at time var optional

--- stytch/oauth.go
+++ stytch/oauth.go
@@ -32,9 +32,9 @@ type OAuthAuthenticateResponse struct {
 }
 
 type ProviderValues struct {
-	AccessToken  string    `json:"access_token,omitempty"`
-	RefreshToken string    `json:"refresh_token,omitempty"`
-	IDToken      string    `json:"id_token,omitempty"`
-	ExpiresAt    time.Time `json:"expires_at,omitempty"`
-	Scopes       []string  `json:"scopes,omitempty"`
+	AccessToken  string     `json:"access_token,omitempty"`
+	RefreshToken string     `json:"refresh_token,omitempty"`
+	IDToken      string     `json:"id_token,omitempty"`
+	ExpiresAt    *time.Time `json:"expires_at,omitempty"`
+	Scopes       []string   `json:"scopes,omitempty"`
 }
