e00f92ce663c0496a3b92ee504c3fc2cebe06b67
Jeremy Kaplan <jeremy@stytch.com>
2023-04-14 14:21:41 -0700

    refactor: Simplify JWKS URL build

--- stytch/b2c/stytchapi/stytchapi_test.go
+++ stytch/b2c/stytchapi/stytchapi_test.go
@@ -97,6 +97,38 @@ func TestNewClient(t *testing.T) {
 			assert.Equal(t, stytcherror.Message("I'm a teapot!"), stytchErr.ErrorMessage)
 		}
 	})
+
+	t.Run("JWKS URL", func(t *testing.T) {
+		httpClient := &http.Client{
+			Transport: RoundTripperFunc(func(r *http.Request) (*http.Response, error) {
+				// Make sure the path got built correctly.
+				if assert.Equal(t, "/v1/sessions/jwks/project-test-00000000-0000-0000-0000-000000000000", r.URL.Path) {
+					resp := &http.Response{
+						StatusCode: http.StatusOK,
+						Body:       io.NopCloser(strings.NewReader(`{"keys": []}`)),
+					}
+					return resp, nil
+				}
+
+				// And then fail it anyway so this doesn't hit the real internet.
+				resp := &http.Response{
+					StatusCode: http.StatusTeapot,
+					Body: io.NopCloser(strings.NewReader(
+						`{"status_code": 418, "error_type": "teapot", "error_message": "I'm a teapot!"}`,
+					)),
+				}
+				return resp, nil
+			}),
+		}
+
+		_, err := stytchapi.NewAPIClient(
+			stytch.EnvTest,
+			"project-test-00000000-0000-0000-0000-000000000000",
+			"secret-test-11111111-1111-1111-1111-111111111111",
+			stytchapi.WithHTTPClient(httpClient),
+		)
+		require.NoError(t, err)
+	})
 }
 
 type RoundTripperFunc func(*http.Request) (*http.Response, error)
