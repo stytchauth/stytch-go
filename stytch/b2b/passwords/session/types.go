09ff9945ac74c5924cbcc7aa2a580f9526324b44
Jennifer McCleary <jennifer@stytch.com>
2024-03-26 16:06:24 -0400

    Add B2B step-up docs and email_id fixes

--- stytch/b2b/passwords/session/types.go
+++ stytch/b2b/passwords/session/types.go
@@ -75,16 +75,15 @@ type ResetResponse struct {
 	// SessionJWT: The JSON Web Token (JWT) for a given Stytch Session.
 	SessionJWT string `json:"session_jwt,omitempty"`
 	// IntermediateSessionToken: The Intermediate Session Token. This token does not necessarily belong to a
-	// specific instance of a Member, but represents a bag of factors that may be converted to a member session.
-	//     The token can be used with the
+	// specific instance of a Member, but represents a bag of factors that may be converted to a member
+	// session. The token can be used with the
 	// [OTP SMS Authenticate endpoint](https://stytch.com/docs/b2b/api/authenticate-otp-sms),
-	// [TOTP Authenticate endpoint](https://stytch.com/docs/b2b/api/authenticate-totp),
-	//     or [Recovery Codes Recover endpoint](https://stytch.com/docs/b2b/api/recovery-codes-recover) to
-	// complete an MFA flow;
-	//     the
+	// [TOTP Authenticate endpoint](https://stytch.com/docs/b2b/api/authenticate-totp), or
+	// [Recovery Codes Recover endpoint](https://stytch.com/docs/b2b/api/recovery-codes-recover) to complete an
+	// MFA flow and log in to the Organization. It can also be used with the
 	// [Exchange Intermediate Session endpoint](https://stytch.com/docs/b2b/api/exchange-intermediate-session)
 	// to join a specific Organization that allows the factors represented by the intermediate session token;
-	//     or the
+	// or the
 	// [Create Organization via Discovery endpoint](https://stytch.com/docs/b2b/api/create-organization-via-discovery) to create a new Organization and Member.
 	IntermediateSessionToken string `json:"intermediate_session_token,omitempty"`
 	// MemberAuthenticated: Indicates whether the Member is fully authenticated. If false, the Member needs to
--- stytch/b2b/sessions/types.go
+++ stytch/b2b/sessions/types.go
@@ -261,17 +261,16 @@ type ExchangeResponse struct {
 	// complete an MFA step to log in to the Organization.
 	MemberAuthenticated bool `json:"member_authenticated,omitempty"`
 	// IntermediateSessionToken: The returned Intermediate Session Token contains any Email Magic Link or OAuth
-	// factors from the original member session that are valid for the target Organization.
-	//       The token can be used with the
-	// [OTP SMS Authenticate endpoint](https://stytch.com/docs/b2b/api/authenticate-otp-sms),
-	// [TOTP Authenticate endpoint](https://stytch.com/docs/b2b/api/authenticate-totp),
-	//       or [Recovery Codes Recover endpoint](https://stytch.com/docs/b2b/api/recovery-codes-recover) to
-	// complete the MFA flow and log in to the target Organization.
-	//       It can also be used with the
+	// factors from the original member session that are valid for the target Organization. If this value is
+	// non-empty, the member must complete an MFA step to finish logging in to the Organization. The token can
+	// be used with the [OTP SMS Authenticate endpoint](https://stytch.com/docs/b2b/api/authenticate-otp-sms),
+	// [TOTP Authenticate endpoint](https://stytch.com/docs/b2b/api/authenticate-totp), or
+	// [Recovery Codes Recover endpoint](https://stytch.com/docs/b2b/api/recovery-codes-recover) to complete an
+	// MFA flow and log in to the Organization. It can also be used with the
 	// [Exchange Intermediate Session endpoint](https://stytch.com/docs/b2b/api/exchange-intermediate-session)
-	// to join a different existing Organization,
-	//       or the
-	// [Create Organization via Discovery endpoint](https://stytch.com/docs/b2b/api/create-organization-via-discovery) to create a new Organization.
+	// to join a specific Organization that allows the factors represented by the intermediate session token;
+	// or the
+	// [Create Organization via Discovery endpoint](https://stytch.com/docs/b2b/api/create-organization-via-discovery) to create a new Organization and Member.
 	IntermediateSessionToken string `json:"intermediate_session_token,omitempty"`
 	// StatusCode: The HTTP status code of the response. Stytch follows standard HTTP response status code
 	// patterns, e.g. 2XX values equate to success, 3XX values are redirects, 4XX are client errors, and 5XX
