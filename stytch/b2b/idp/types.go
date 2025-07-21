3e7005f21309279f2ede68d6e6e91699a4afc6e4
Max Gerber <max@stytch.com>
2025-07-21 16:50:10 -0700

    feat: [AUTH-5632] Add IntrospectTokenNetwork and fix IntrospectTokenLocal

--- stytch/b2b/idp/types.go
+++ stytch/b2b/idp/types.go
@@ -1,9 +1,9 @@
 package idp
 
 import (
+	"github.com/golang-jwt/jwt/v5"
 	"time"
 
-	"github.com/golang-jwt/jwt/v5"
 	"github.com/stytchauth/stytch-go/v16/stytch/b2b/sessions"
 )
 
@@ -12,20 +12,38 @@ type OrganizationClaim struct {
 	Slug           string `json:"slug"`
 }
 
-type IntrospectTokenClaims struct {
-	Subject      string            `json:"sub"`
-	Scope        string            `json:"scope"`
-	CustomClaims map[string]any    `json:"custom_claims"`
-	ExpiresAt    int32             `json:"exp"`
-	IssuedAt     int32             `json:"iat"`
-	NotBefore    int32             `json:"nbf"`
+type IntrospectTokenResponse struct {
+	Active       bool              `json:"active"`
 	TokenType    string            `json:"token_type"`
+	Issuer       string            `json:"iss"`
+	Subject      string            `json:"sub"`
+	Audience     []string          `json:"aud"`
+	Scope        string            `json:"scope"`
+	ClientID     string            `json:"client_id"`
+	Expiry       *jwt.NumericDate  `json:"exp"`
+	IssuedAt     *jwt.NumericDate  `json:"iat"`
 	Organization OrganizationClaim `json:"https://stytch.com/organization"`
+	CustomClaims map[string]any
+}
+
+type IntrospectTokenClaims struct {
+	ClientID     string            `json:"client_id"`
+	Scope        string            `json:"scope"`
+	Organization OrganizationClaim `json:"https://stytch.com/organization"`
+	JTI          string            `json:"jti"`
 	jwt.RegisteredClaims
 }
 
 type IntrospectTokenLocalParams struct {
 	Token              string
 	MaxTokenAge        time.Duration
+	AuthorizationCheck *sessions.AuthorizationCheck
+}
+
+type IntrospectTokenNetworkParams struct {
+	Token              string
+	ClientID           string
+	ClientSecret       *string
+	TokenTypeHint      *string
 	AuthorizationCheck *sessions.AuthorizationCheck
 }
